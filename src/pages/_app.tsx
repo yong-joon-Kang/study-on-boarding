9afb3d8c0da6110287a06b14e2cb3cea4f861316
DESKTOP-3GEP8UR\kangy <kangyj2197@gmail.com>
2023-07-26 07:23:11 +0900

    [ADD] app.tsx에 redux 적용, layout 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+// import '@/styles/globals.css'
+import type { AppProps } from "next/app";
+import React from "react";
+import { Provider } from "react-redux";
+import store from "../commons/library/redux/store";
+import Layout from "@/components/layout";
+import "../styles/myGlobal.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
+  );
 }
