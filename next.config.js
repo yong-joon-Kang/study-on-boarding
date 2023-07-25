e6fad3d5128901f1a3ce0a2113c2944563b656be
DESKTOP-3GEP8UR\kangy <kangyj2197@gmail.com>
2023-07-26 07:20:08 +0900

    [MODIFY] 첫페이지 렌더링 변경

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  exportPathMap: function () {
+    return {
+      "/": {
+        page: "/products/list",
+        query: { __nextDefaultLocale: "en" },
+      }, // '/' 경로 대신 '/beautyShop/list'로 첫 페이지를 설정
+    };
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
