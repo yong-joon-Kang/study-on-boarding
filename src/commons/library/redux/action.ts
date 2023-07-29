0fb6cab85f900fe36ecb7240f4b9f81c7cca5a1a
DESKTOP-3GEP8UR\kangy <kangyj2197@gmail.com>
2023-07-30 01:25:41 +0900

    [ADD] 징비구니에 담기는 상품들에 대한 redux action setting

--- src/commons/library/redux/action.ts
+++ src/commons/library/redux/action.ts
@@ -1,10 +1,18 @@
 // 액션 타입 정의
 export const SET_PRODUCTS = "SET_PRODUCTS";
+export const SET_CART = "SET_CART";
 
 // 액션 생성자 함수
 export const setProducts = (products: any) => {
   return {
     type: SET_PRODUCTS,
     payload: products,
+  };
+};
+
+export const setCart = (cart: any) => {
+  return {
+    type: SET_CART,
+    payload: cart,
   };
 };
